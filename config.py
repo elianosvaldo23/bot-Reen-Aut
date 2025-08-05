dc4612737acb0d2fc166d3ca2afc84c8110df2b9
elianosvaldo23 <elianverdeciamesa@gmail.com>
2025-08-05 19:27:19 -0400

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ BOT_TOKEN = os.getenv('BOT_TOKEN', '8063509725:AAHsa32julaJ4fst2OWhgj7lkL_HdA5AL
 ADMIN_ID = int(os.getenv('ADMIN_ID', '1742433244'))
 
 DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///auto_post_bot.db')
+TIMEZONE = os.getenv('TIMEZONE', 'UTC')
 
 MAX_POSTS = 5
 MAX_CHANNELS_PER_POST = 50
