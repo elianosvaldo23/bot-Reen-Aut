8560fdd93efff58479f7efb1562943577d0e2ea9
elianosvaldo23 <elianverdeciamesa@gmail.com>
2025-08-06 16:02:42 -0400

    Actualizar config.py

--- config.py
+++ config.py
@@ -6,8 +6,11 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN', '8063509725:AAHsa32julaJ4fst2OWhgj7lkL_HdA5ALN4')
 ADMIN_ID = int(os.getenv('ADMIN_ID', '1742433244'))
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///auto_post_bot.db')
+# MongoDB Configuration
+MONGODB_URL = os.getenv('MONGODB_URL', 'mongodb+srv://zoobot:zoobot@zoolbot.6avd6qf.mongodb.net/zoolbot?retryWrites=true&w=majority&appName=Zoolbot')
+DATABASE_NAME = os.getenv('DATABASE_NAME', 'zoolbot')
+
 TIMEZONE = os.getenv('TIMEZONE', 'UTC')
 
-MAX_POSTS = 5
-MAX_CHANNELS_PER_POST = 50
+MAX_POSTS = 15
+MAX_CHANNELS_PER_POST = 90
