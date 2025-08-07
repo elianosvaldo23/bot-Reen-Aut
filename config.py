c407455b268f9479bd9855d3d096bc5eb4bcacd0
elianosvaldo23 <elianverdeciamesa@gmail.com>
2025-08-06 21:44:54 -0400

    Actualizar config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv('BOT_TOKEN', '8063509725:AAHsa32julaJ4fst2OWhgj7lkL_HdA5ALN4')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '8461754250:AAHHAcEyJyzGWguqRDrdvouwTUo2jbpryGs')
 ADMIN_ID = int(os.getenv('ADMIN_ID', '1742433244'))
 
 # MongoDB Configuration
